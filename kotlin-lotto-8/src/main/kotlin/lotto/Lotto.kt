685d96e56be2c72ad4f111d305901e2c33b26b23
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-09 18:49:53 +0900

    init: 3주차 과제

---
+++ kotlin-lotto-8/src/main/kotlin/lotto/Lotto.kt
@@ -0,0 +1,9 @@
+package lotto
+
+class Lotto(private val numbers: List<Int>) {
+    init {
+        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+    }
+
+    // TODO: 추가 기능 구현
+}
---
+++ kotlin-lotto-8/src/test/kotlin/lotto/LottoTest.kt
@@ -0,0 +1,23 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class LottoTest {
+    @Test
+    fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+        }
+    }
+
+    // TODO: 테스트가 통과하도록 프로덕션 코드 구현
+    @Test
+    fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 5))
+        }
+    }
+
+    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+}
